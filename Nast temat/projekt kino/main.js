b55ca475e287c807d6d80f0f1218243912d649a2
wolnik-kamil <112876137+wolnik-kamil@users.noreply.github.com>
2023-11-08 17:59:50 +0100

    Add files via upload

---
+++ Nast temat/projekt kino/main.js
@@ -0,0 +1,9 @@
+// function bookseat() {
+//     const clickCouch = document.getElementsByClassName("couch");
+//     clickCouch.classList.add("red");
+// }
+
+const seat = document.querySelector('#seat')
+const row = document.querySelector('#row')
+const btn = document.querySelectorAll('button')
+
